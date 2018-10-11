8a5007916b745499979eeb9bcd08b45481cf4fd8
ctiautomation <43128892+ctiautomation@users.noreply.github.com>
2018-10-11 11:24:59 +0530

    all changes
    Commit this

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace CTITopicModelingAPI
+{
+    public static class WebApiConfig
+    {
+        /// <summary>
+        /// Register - Handling routing
+        /// </summary>
+        /// <param name="config"></param>
+        public static void Register(HttpConfiguration config)
+        {          
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
